c1cf34372b6fccdf006887ba7471c30464978544
Tanti <tantirestup@gmail.com>
2023-04-01 08:25:37 +0700

    commit

--- script.js
+++ script.js
@@ -56,13 +56,13 @@ const calculate = () =>{
         result = parseFloat (prevNumber) + parseFloat  (currentNumber);
         break;
         case '-':
-        result = parseFloat  (prevNumber) - pparseFloat  (currentNumber);
+        result = parseFloat  (prevNumber) - parseFloat  (currentNumber);
         break;
         case '*':
-        result =   (prevNumber) *   (currentNumber);
+        result =  parseFloat (prevNumber) *  parseFloat (currentNumber);
         break;
         case '/':
-        result =   (prevNumber) /   (currentNumber);
+        result =  parseFloat (prevNumber) /  parseFloat (currentNumber);
         break;
         default:
             return
